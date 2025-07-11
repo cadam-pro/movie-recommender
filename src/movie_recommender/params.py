d87c17540cd44e3f483ba209abdf108cd118561d
Corentin Adam <cor.adam@laposte.net>
2025-07-11 13:21:39 +0000

    [ADD] clean_data function

--- src/movie_recommender/params.py
+++ src/movie_recommender/params.py
@@ -3,3 +3,5 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FOLDER = os.path.abspath(os.path.join(BASE_DIR, "../..", "data"))
 DATA_PATH = os.path.join(DATA_FOLDER, "TMDB_all_movies.csv")
+
+CHECK_DUPLICATION_FEATURES = ["release_date", "overview"]
